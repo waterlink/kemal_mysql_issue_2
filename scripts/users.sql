3fa8e520091b2ce886994b2dbbd6706fb21c60c6
Oleksii Fedorov <waterlink000@gmail.com>
2016-03-18 00:18:30 +0100

    Add code for a second case

--- scripts/users.sql
+++ scripts/users.sql
@@ -1,8 +1,11 @@
+DROP TABLE IF EXISTS users;
+
 CREATE TABLE IF NOT EXISTS users (
   id int(11) NOT NULL AUTO_INCREMENT,
   username varchar(255) NOT NULL,
+  name varchar(100) DEFAULT NULL,
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=3;
 
-INSERT INTO users (username) VALUES ("john.smith");
-INSERT INTO users (username) VALUES ("sarah.connor");
+INSERT INTO users (username, name) VALUES ("john.smith", "John");
+INSERT INTO users (username, name) VALUES ("sarah.connor", "Sarah");
